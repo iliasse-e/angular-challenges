ee63083e0fd9525f766355baacbd5d94b7ca0047
Iliasse EL ABOUYI <E678E22@adsn.fr>
2024-08-17 13:58:25 +0200

    refactor(pure-pipe-easy): migrate to pipe instead of function

--- apps/angular/pipe-easy/src/app/app.component.ts
+++ apps/angular/pipe-easy/src/app/app.component.ts
@@ -1,21 +1,23 @@
 import { NgFor } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({ name: 'index', standalone: true })
+export class IndexPipe implements PipeTransform {
+  transform(value: string, index: number): string {
+    return `${value} - ${index}`;
+  }
+}
 
 @Component({
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor, IndexPipe],
   selector: 'app-root',
   template: `
     <div *ngFor="let person of persons; let index = index">
-      {{ heavyComputation(person, index) }}
+      {{ person | index: index }}
     </div>
   `,
 })
 export class AppComponent {
-  persons = ['toto', 'jack'];
-
-  heavyComputation(name: string, index: number) {
-    // very heavy computation
-    return `${name} - ${index}`;
-  }
+  persons: string[] = ['toto', 'jack'];
 }
